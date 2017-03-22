126b670348d0e23dd18b4e72e9e291ad1674cb10
keatonwalker <keatwalker@gmail.com>
2017-03-22 13:30:30 -0600

    Bootstrapping spec json implemented

--- sde-package.py
+++ sde-package.py
@@ -7,15 +7,20 @@ from time import clock
 from hashlib import md5
 from xxhash import xxh32
 import json
+import io
+import ntpath
 
 from apiclient import errors
-from apiclient.http import MediaFileUpload
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
 import httplib2
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
+
+HASH_DRIVE_FOLDER = '0B3wvsjTJuTRQMmJPSVkwanFqeHc'
+UTM_DRIVE_FOLDER = '0B3wvsjTJuTRQX1VldnFGS0pqZ2s'
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/drive'
@@ -176,9 +181,15 @@ def zip_folder(folder_path, zip_name):
         original_size += info.file_size
         compress_size += info.compress_size
     zf.close()
-    print '{} Original size: {} KB'.format(zip_name, original_size / 1000)
-    print '{} Compresses size: {} KB'.format(zip_name, compress_size / 1000)
-    pass
+    print '{} Original / Compressed size: {} / {} MB'.format(ntpath.basename(zip_name),
+                                                             original_size / 1000000.0,
+                                                             compress_size / 1000000.0)
+    # print '{} Compresses size: {} KB'.format(zip_name, compress_size / 1000)
+
+
+def unzip(zip_path, output_path):
+    with zipfile.ZipFile(zip_path, 'r', zipfile.ZIP_DEFLATED) as zipped:
+        zipped.extractall(output_path)
 
 
 def get_hash_lookup(hash_path, hash_field):
@@ -240,6 +251,7 @@ def detect_changes(data_path, fields, past_hashes, output_fc, output_hashes, sha
         attribute_subindex = -3
 
     hashes = {}
+    changes = 0
     with arcpy.da.SearchCursor(data_path, cursor_fields) as cursor, \
             arcpy.da.InsertCursor(output_fc, fields) as ins_cursor, \
             open(hash_store, 'wb') as hash_csv:
@@ -265,7 +277,8 @@ def detect_changes(data_path, fields, past_hashes, output_fc, output_hashes, sha
                 ins_cursor.insertRow(row[:attribute_subindex])
 
                 if digest not in past_hashes:
-                    print 'OID {} is an update'.format(oid)
+                    changes += 1
+    print 'Total changes: {}'.format(changes)
 
 
 def create_formatted_outputs(output_directory, input_feature, output_name):
@@ -317,14 +330,14 @@ def create_formatted_outputs(output_directory, input_feature, output_name):
                 attribute_hash_digest = _create_hash(str(row[:-2]), unique_salty_id)
                 hash_writer.writerow((src_id, geom_hash_digest, attribute_hash_digest))
                 ins_cursor.insertRow(row[:-1])
-    print 'hash ins time: {}'.format(clock() - hash_ins_time)
+    # print 'hash ins time: {}'.format(clock() - hash_ins_time)
     # Create shape file
     arcpy.CopyFeatures_management(output_fc, output_shape)
 
     return (output_gdb, shape_directory, hash_directory)
 
 
-def create_changes_and_outputs(output_directory, input_feature, output_name, past_hashes_path, hash_field):
+def create_changes_and_outputs(output_directory, input_feature, output_name, past_hashes, hash_field):
     input_desc = arcpy.Describe(input_feature)
     spatial_ref = input_desc.spatialReference
     geo_type = input_desc.shapeType
@@ -356,61 +369,203 @@ def create_changes_and_outputs(output_directory, input_feature, output_name, pas
     # sql_clause = (None, 'ORDER BY {}'.format('OBJECTID'))
     # unique_salty_id = 0
     hash_ins_time = clock()
-    past_hashes = get_hash_lookup(past_hashes_path, hash_field)
+    # past_hashes = get_hash_lookup(past_hashes_path, hash_field)
 
     detect_changes(input_feature, fields, past_hashes, output_fc, hash_store, 'SHAPE@WKT')
-    print 'hash ins time: {}'.format(clock() - hash_ins_time)
+    #print 'hash ins time: {}'.format(clock() - hash_ins_time)
     # Create shape file
     arcpy.CopyFeatures_management(output_fc, output_shape)
 
     return (output_gdb, shape_directory, hash_directory)
 
 
-if __name__ == '__main__':
-    # # ************ create zip from sde section
-    output_directory = r'C:\GisWork\drive_sgid\test_outputs'
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+def download_zip(file_id, service, output):
+    import shutil
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print "Download %d%%." % int(status.progress() * 100)
+    fh.seek(0)
+    with open(output, 'wb') as out_zip:
+        shutil.copyfileobj(fh, out_zip, length=131072)
+    print 'done'
+
+
+def update_file(file_id, local_file, drive_service):
+    media_body = MediaFileUpload(local_file,
+                                 mimetype='application/zip',
+                                 resumable=True)
+
+    # file = drive_service.files().get(fileId=self.file_id, fields='').execute()
+    # print file
+    # file_metadata = {'name': self.name}
+
+    # request = service.files().create(body=file_metadata,
+    #                                  media_body=media_body,
+    #                                  fields="id")
+    request = drive_service.files().update(fileId=file_id,
+                                           media_body=media_body)
+
+    response = None
+    while response is None:
+        status, response = request.next_chunk()
+    #   if status :
+    #     print('{} percent {}'.format(name, int(status.progress() * 100)))
+
+    return response.get('id')
+
+
+def create_drive_zip(name, parent_ids, local_file, service):
+
+    file_metadata = {'name': name,
+                     'mimeType': 'application/zip',
+                     'parents': parent_ids}
+
+    media_body = MediaFileUpload(local_file,
+                                 mimetype='application/zip',
+                                 resumable=True)
+    request = service.files().create(body=file_metadata,
+                                     media_body=media_body,
+                                     fields="id")
+    response = None
+    while response is None:
+        status, response = request.next_chunk()
+    #   if status :
+    #     print('{} percent {}'.format(name, int(status.progress() * 100)))
+
+    return response.get('id')
+
+
+def get_file_id_by_name_and_directory(name, parent_id, service):
+    response = service.files().list(q="name='{}' and '{}' in parents  and explicitlyTrashed=false".format(name,
+                                                                                                          parent_id),
+                                    spaces='drive',
+                                    fields='files(id)').execute()
+    files = response.get('files', [])
+    if len(files) > 0:
+        return files[0].get('id')
     else:
-        shutil.rmtree(output_directory)
-        print 'directory removed'
-        os.makedirs(output_directory)
+        return None
 
-    input_feature = r'Database Connections\Connection to sgid.agrc.utah.gov.sde\SGID10.Recreation.Trails'
-    output_name = input_feature.split('.')[-1]
 
-    fields = _filter_fields([fld.name for fld in arcpy.ListFields(input_feature)])
+def create_drive_folder(name, parent_ids, service):
+    # existing_file_id = get_file_id_by_name_and_directory(name, parent_ids[0], service)
+    # if existing_file_id:
+    #     print 'Existing file'
+    #     return existing_file_id
+        # raise Exception('Drive folder {} already exists at: {}'.format(name, existing_file_id))
 
-    hash_directory = r'C:\GisWork\drive_sgid\past_hashes'
-    past_hash_store = os.path.join(hash_directory, 'Trails_pasthashes.csv')
+    file_metadata = {'name': name,
+                     'mimeType': 'application/vnd.google-apps.folder',
+                     'parents': parent_ids}
+
+    response = service.files().create(body=file_metadata,
+                                      fields="id").execute()
+
+    return response.get('id')
+
+
+def load_feature_json(json_path):
+    with open(json_path, 'r') as json_file:
+        feature = json.load(json_file)
+
+    return feature
+
+
+if __name__ == '__main__':
+    drive_service = setup_drive_service()
+    # -------------Set these to test--------------------
+    workspace = r'Database Connections\Connection to sgid.agrc.utah.gov.sde'
+    feature_name = 'SGID10.RECREATION.Trails'
+
+    output_directory = r'package_temp'
+    empty_spec = os.path.join('features', 'template.json')
+
+    def renew_temp_directory(directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        else:
+            shutil.rmtree(directory)
+            print 'Temp directory removed'
+            os.makedirs(directory)
+
+    renew_temp_directory(output_directory)
+
+    input_feature_path = os.path.join(workspace, feature_name)
+    spec_name = '_'.join(feature_name.split('.')[-2:]) + '.json'
+    feature_spec = os.path.join('features', spec_name)
+
+    feature = None
+    if not os.path.exists(feature_spec):
+        feature = load_feature_json(empty_spec)
+        feature['sgid_name'] = feature_name
+        feature['name'] = input_feature_path.split('.')[-1]
+        feature['category'] = input_feature_path.split('.')[-2]
+    else:
+        feature = load_feature_json(feature_spec)
+
+    # Check for category folder
+    category_id = get_file_id_by_name_and_directory(feature['category'], UTM_DRIVE_FOLDER, drive_service)
+    if not category_id:
+        print 'Creating drive folder {}'.format(feature['category'])
+        temp_folder_id = create_drive_folder(feature['category'], [UTM_DRIVE_FOLDER], drive_service)
+        feature['parent_ids'].append(temp_folder_id)
+
+    output_name = feature['name']
+    fields = _filter_fields([fld.name for fld in arcpy.ListFields(input_feature_path)])
+
+    # Get the last hash from drive to check changes
+    past_hash_directory = os.path.join(output_directory, 'pasthashes')
     hash_field = 'hash'
+    past_hash_zip = os.path.join(output_directory, 'TrailsDown.zip')
+    past_hash_store = os.path.join(past_hash_directory, output_name + '_hash', output_name + '_hashes.csv')
+    past_hashes = None
+    if feature['hash_id']:
+        download_zip(feature['hash_id'], drive_service, past_hash_zip)
+        print 'Past hashes downloaded'
+        unzip(past_hash_zip, past_hash_directory)
+        past_hashes = get_hash_lookup(past_hash_store, hash_field)
+    else:
+        past_hashes = {}
 
-    #create_hash_table(input_feature, fields, past_hash_store, 'SHAPE@WKT')
+    # Copy data local and check for changes
     fc_directory, shape_directory, hash_directory = create_changes_and_outputs(
                                                                              output_directory,
-                                                                             input_feature,
+                                                                             input_feature_path,
                                                                              output_name,
-                                                                             past_hash_store,
+                                                                             past_hashes,
                                                                              hash_field)
 
-    # # Zip up outputs
-    # zip_folder(fc_directory, os.path.join(output_directory, '{}_gdb.zip'.format(output_name)))
-    # zip_folder(shape_directory, os.path.join(output_directory, '{}_shp.zip'.format(output_name)))
-    # zip_folder(hash_directory, os.path.join(output_directory, '{}_hash.zip'.format(output_name)))
+    def load_zip_to_drive(id_key, new_zip, parent_folder_ids):
+        if feature[id_key]:
+            update_file(feature[id_key], new_zip, drive_service)
+        else:
+            temp_id = create_drive_zip(ntpath.basename(new_zip),
+                                       parent_folder_ids,
+                                       new_zip,
+                                       drive_service)
+            feature[id_key] = temp_id
+    # Zip up outputs
+    new_gdb_zip = os.path.join(output_directory, '{}_gdb.zip'.format(output_name))
+    new_shape_zip = os.path.join(output_directory, '{}_shp.zip'.format(output_name))
+    new_hash_zip = os.path.join(output_directory, '{}_hash.zip'.format(output_name))
 
-    # ************** read setting stuff from json files section
-    # json_file = './data/fileids_20161215_141700.json'
-    # path_drivefiles = load_path_ids(json_file)
-    # unpackaged_drivefiles = get_unpackaged_drivefiles_by_name(path_drivefiles)
-    # print len(unpackaged_drivefiles)
+    zip_folder(fc_directory, new_gdb_zip)
+    zip_folder(shape_directory, new_shape_zip)
+    zip_folder(hash_directory, new_hash_zip)
+    # Upload to drive
+    load_zip_to_drive('gdb_id', new_gdb_zip, feature['parent_ids'])
+    print 'GDB loaded'
+    load_zip_to_drive('shape_id', new_shape_zip, feature['parent_ids'])
+    print 'Shape loaded'
+    load_zip_to_drive('hash_id', new_hash_zip, [HASH_DRIVE_FOLDER])
+    print 'Hash loaded'
 
-    # ************** update drive file section
-    # test_file = DriveFile('fuckerduckery/Trails_gdb.zip',
-    #                       '0B3wvsjTJuTRQMHhWY2JlLW9iSnM',
-    #                       '0B3wvsjTJuTRQOXNRVTQ2NC1ubFU')
-    # drive_service = setup_drive_service()
-    #
-    # uf = test_file.update_file(r'/Volumes/C/GisWork/drive_sgid/test_outputs/Trails_gdb.zip', drive_service)
-    # print uf
+    with open(feature_spec, 'w') as f_out:
+        f_out.write(json.dumps(feature, sort_keys=True, indent=4))
+    # print json.dumps(feature, sort_keys=True, indent=4)
 
-    print 'Completed'
+    print 'Complete!'
